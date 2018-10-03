2f921ba90cf92fdb204c8e8ef2ef3d347b425463
Joel Rannebarger <dev@joelrann.net>
2018-10-03 17:17:21 -0400

    State, Props and Style

--- components/BasicText.js
+++ components/BasicText.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Text } from 'react-native';
 
 const BasicText = ({value}) => (
     <Text>{value}</Text>
 );
+
+BasicText.propTypes = {
+    value: PropTypes.string,
+};
+
+
+BasicText.defaultProps = {
+    value: 'Nothing Here',
+};
 
 export default BasicText;
--- screens/HappyScreen.js
+++ screens/HappyScreen.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import BasicText from '../components/BasicText';
 
 class HappyScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayValue: 'Hello World',
+        };
+    }
+
+    onTextChange = (value) => {
+        this.setState({ displayValue: value});
+        console.log(`Text Change: ${value} `);
+    };
 
     render() {
+        const { displayValue } = this.state;
+
         return (
-            <View>
-                <BasicText value="Hello World"/>
+            <View style={styles.container}>
+                <BasicText value={displayValue} />
+                <TextInput
+                    value={displayValue}
+                    placeholder="Enter Some Text"
+                    onChangeText={this.onTextChange}
+                    style={styles.textInput}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    textInput: {
+        marginTop: 20,
+        borderWidth: 1,
+    },
+});
 
 export default HappyScreen;
